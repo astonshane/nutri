47b4962a1305fa0d347b31e42c2bb415c27b24c4
Shane Aston <astonshane@gmail.com>
2025-08-09 17:38:17 -0400

    cleanup

--- nutri/fatsecret/serving.py
+++ nutri/fatsecret/serving.py
@@ -10,12 +10,3 @@ class Serving:
         self.nutrition_info = {}
         for key in static_nutrition_info.keys():
             self.nutrition_info[key] = float(body.get(key, 0))
-
-        self.calories = float(body.get('calories', 0))
-        self.carbohydrate = float(body.get('carbohydrate', 0))
-        self.protein = float(body.get('protein', 0))
-        self.fat = float(body.get('fat', 0))
-        self.cholesterol = float(body.get('cholesterol', 0))
-        self.sodium = float(body.get('sodium', 0))
-        self.fiber = float(body.get('fiber', 0))
-        self.sugar = float(body.get('sugar', 0))
